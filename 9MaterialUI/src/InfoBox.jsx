8f593c3da62aa9880331f8c1cffe17587ea4b4fe
Ankitrwat2004 <ankitrawat3700@gmail.com>
2025-02-25 13:43:08 +0530

    SOEM CHANGES

--- 9MaterialUI/src/InfoBox.jsx
+++ 9MaterialUI/src/InfoBox.jsx
@@ -8,7 +8,6 @@ import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
 import "./infoBox.css";
 
 export default function InfoBox({info}){
-    const INIT_URL="https://media.istockphoto.com/id/2175083463/photo/new-delhi-delhi-india-fire-truck-spraying-water-over-delhi-streets-amid-pollution-emergency.webp?a=1&b=1&s=612x612&w=0&k=20&c=M-M-0G2h5claKSoJDulPHau74xPvqheMVp7Xg0WjaZ8=";
     const HOT_URL="https://media.istockphoto.com/id/824845572/photo/thermometer-sun-high-degres-hot-summer-day-high-summer-temperatures.webp?a=1&b=1&s=612x612&w=0&k=20&c=DPDwtq156djUSKSd57TR23BmU4vzC8xqolwMkqHCdfU=";
     const COLD_URL="https://images.unsplash.com/photo-1541565437827-5f8a6f8a1b61?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Q09MRCUyMElNQUdFfGVufDB8fDB8fHww";
     const RAIN_URL="https://plus.unsplash.com/premium_photo-1733436275328-bbb7da0fe4ae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8UkFJTlklMjBXRUFUSEVSJTIwJTIwSU1BR0V8ZW58MHx8MHx8fDA%3D";
@@ -42,15 +41,14 @@ export default function InfoBox({info}){
         <p>humidity={info.humidity}</p>
         <p>Min Temp={info.tempMin}</p>
         <p>Max Temp={info.tempMax}</p>
-       <p>The weather  can  be describe as <i>{info.weather}</i> and  feelslike {info.feelslike} &deg;C</p>
+       <p>The weather  can  be describe as {info.weather}and  feelslike {info.feelslike} &deg;C</p>
 
         </Typography>
       </CardContent>
-    
     </Card>
     </div>
         </div>
-    )
+    );
 }
 
 
